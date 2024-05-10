70d978b94cf2e71edaa3b7d0a6481ab30512dd01
Evgeny Bushuev <fakevkbushmen@gmail.com>
2024-05-10 21:54:07 +0500

    Fix TuneRadioScenario

--- Assets/Scripts/Scenario/TuneRadioScenario.cs
+++ Assets/Scripts/Scenario/TuneRadioScenario.cs
@@ -1,38 +1,60 @@
+using System;
 using System.Collections;
 using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class TuneRadioScenario : BaseScenario
 {
-	[SerializeField]
-	private float _afterStartDelay;
+    [SerializeField]
+    private float _afterStartDelay;
+    [SerializeField]
+    protected Trigger _completeMiniGameTrigger;
 
-	[SerializeField]
-	private PlayVoice _instructionVoice;
+    [SerializeField]
+    private PlayVoice _instructionVoice;
     [SerializeField]
     private TuneRadioGame _tuneRadioGame;
 
     [SerializeField]
     private InteractableWithTrigger _radioInteractableTrigger;
 
+    private void Start()
+    {
+        Trigger.OnTriggerInvoke += OnTriggered;
+    }
+
+    private void OnDestroy()
+    {
+        Trigger.OnTriggerInvoke -= OnTriggered;
+    }
+
+    private void OnTriggered(Trigger trigger)
+    {
+        if (trigger == _completeMiniGameTrigger)
+        {
+            _questController.CompleteQuest(QuestsDescriptionContainer.RADIO_CHECK);
+            Debug.Log("TuneRadioScenario finished");
+            Finish();
+            Trigger.OnTriggerInvoke -= OnTriggered;
+        }
+    }
+
     public override void Run()
-	{
-		if (_isScenarioStarted) return;
+    {
+        if (_isScenarioStarted) return;
         base.Run();
-		StartCoroutine(StartGame());
-	}
+        StartCoroutine(StartGame());
+    }
 
-	private IEnumerator StartGame()
-	{
-		Debug.Log("TuneRadioScenario start");
-		yield return new WaitForSeconds(_afterStartDelay);
-		yield return _instructionVoice.Play();
+    private IEnumerator StartGame()
+    {
+        Debug.Log("TuneRadioScenario start");
+        yield return new WaitForSeconds(_afterStartDelay);
+        yield return _instructionVoice.Play();
 
-		_tuneRadioGame.EnableNoise();
+        _tuneRadioGame.EnableNoise();
 
         _questController.AddQuest(QuestsDescriptionContainer.RADIO_CHECK);
         _radioInteractableTrigger.IsAvailableNow = true;
-        Debug.Log("TuneRadioScenario finished");
-        Finish();
-	}
+    }
 }
