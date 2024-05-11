0c3cc779e1bd13d0af5b64003dc2507b3d2c9d38
Stanislav Bukotkin <djexstas9@gmail.com>
2024-05-11 12:54:19 +0500

    Add wife scenario

---
+++ Assets/Scripts/Scenario/WifeScenario.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class WifeScenario : BaseScenario
+{
+    [SerializeField]
+    private PlayVoice _notificationSystemVoice;
+    
+    [SerializeField]
+    private PlayVoice _wifeVoice;
+
+    [SerializeField]
+    private float _delayBetweenVoices = 0.5f;
+
+    [SerializeField]
+    private float _delayAfterWife = 2f;
+
+    [SerializeField]
+    private bool _addQuestBadge = true;
+    
+    public override async void Run()
+    {
+        base.Run();
+        
+        try
+        {
+            await _notificationSystemVoice.Play().ToUniTask(cancellationToken: this.GetCancellationTokenOnDestroy());
+            await UniTask.Delay(TimeSpan.FromSeconds(_delayBetweenVoices), cancellationToken: this.GetCancellationTokenOnDestroy());
+            await _wifeVoice.Play().ToUniTask(cancellationToken: this.GetCancellationTokenOnDestroy());
+
+            if (_addQuestBadge)
+            {
+                _questController.AddQuest(QuestsDescriptionContainer.CALL_BACK);
+            }
+            
+            await UniTask.Delay(TimeSpan.FromSeconds(_delayAfterWife), cancellationToken: this.GetCancellationTokenOnDestroy());
+        }
+        catch (OperationCanceledException e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
